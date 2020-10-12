313481126031cc883386d39006ec8fc1100171bc
samsung <samsung@samsung-PC>
2020-10-12 15:44:38 +0530

    First

---
+++ HandlingJava/src/BasicProgram.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class BasicProgram {
+	
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+System.out.println("its a main function");
+int y=doLogout(23,45);
+System.out.println(y);
+doLogout(45,56);
+doLogout(48,67);
+	}
+	public static int doLogout(int x,int y)
+	{
+		int z=x+y;
+		System.out.println(z);
+		System.out.println("its a logout");
+		return z;
+	}
+	public static void doLogin()
+	{
+		System.out.println("its a login");
+	}
+	public static void forgotPassword()
+	{
+		System.out.println("its a forgot password");
+
+ 
+ 
+	 }
+ 
+
+			
+		}
+
+
+	
+		
+
+
+	
+
+
+	
+
+
+
+
+
+
+	
+
+
---
+++ HandlingJava/src/HandlingConstructor.java
@@ -0,0 +1,18 @@
+
+public class HandlingConstructor {
+static int x;
+static String y;
+HandlingConstructor()
+{
+	System.out.println("its a Constructor block");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HandlingConstructor v=new HandlingConstructor();
+		System.out.println("its a main method");
+		System.out.println(y);
+		
+
+	}
+
+}
