8054e8c78142cd19a03f3074b28cee9f0c1fa4fe
samsung <samsung@samsung-PC>
2020-10-16 17:55:06 +0530

    removed 4th sysout

--- HandlingJava/src/BasicProgram.java
+++ HandlingJava/src/BasicProgram.java
@@ -16,26 +16,9 @@ public class BasicProgram {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-System.out.println("its a main function");
-int y=doLogout(23,45);
-System.out.println(y);
-doLogout(45,56);
-doLogout(48,67);
-	}
-	public static int doLogout(int x,int y)
-	{
-		int z=x+y;
-		System.out.println(z);
-		System.out.println("its a logout");
-		return z;
-	}
-	public static void doLogin()
-	{
-		System.out.println("its a login");
-	}
-	public static void forgotPassword()
-	{
-		System.out.println("its a forgot password");
+System.out.println("i love nayanthara");
+System.out.println("i love trisha");
+System.out.println("i love keerthisuresh");
 
  
  
