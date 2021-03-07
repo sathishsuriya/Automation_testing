3e75eb0b3046e4b31675b116e5145c03e4236a05
samsung <samsung@samsung-PC>
2021-03-07 12:04:10 +0530

    removed 4th sysout

---
+++ ClProjectfour/src/ClSampleScript.java
@@ -0,0 +1,12 @@
+
+public class ClSampleScript {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("continous integration");
+System.out.println("selenium");
+System.out.println("java");
+System.out.println("ashwin");
+	}
+
+}
